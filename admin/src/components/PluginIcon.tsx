437eb924d730aaeba470c5a4d144f10013a016b0
Patiparnne <partyparti@gmail.com>
2026-06-08 11:29:11 +0700

    chore: bump version to 0.0.3 and update dependencies
    - Removed specific dependencies from package.json
    - Updated peerDependencies to allow broader version ranges

--- admin/src/components/PluginIcon.tsx
+++ admin/src/components/PluginIcon.tsx
@@ -1,5 +1,10 @@
-import { PuzzlePiece } from '@strapi/icons';
-
-const PluginIcon = () => <PuzzlePiece />;
+const PluginIcon = () => (
+  <svg aria-hidden="true" fill="none" height="1em" viewBox="0 0 24 24" width="1em">
+    <path
+      d="M12 3.5a4 4 0 0 0-4 4v2H6.75A2.25 2.25 0 0 0 4.5 11.75v6A2.25 2.25 0 0 0 6.75 20h10.5a2.25 2.25 0 0 0 2.25-2.25v-6a2.25 2.25 0 0 0-2.25-2.25H16v-2a4 4 0 0 0-4-4Zm2.5 6v-2a2.5 2.5 0 0 0-5 0v2h5ZM12 14a1.25 1.25 0 0 0-.75 2.25v1a.75.75 0 0 0 1.5 0v-1A1.25 1.25 0 0 0 12 14Z"
+      fill="currentColor"
+    />
+  </svg>
+);
 
 export { PluginIcon };
--- admin/src/pages/HomePage.tsx
+++ admin/src/pages/HomePage.tsx
@@ -1,15 +1,8 @@
-import { Main } from '@strapi/design-system';
-import { useIntl } from 'react-intl';
-
-import { getTranslation } from '../utils/getTranslation';
-
 const HomePage = () => {
-  const { formatMessage } = useIntl();
-
   return (
-    <Main>
-      <h1>Welcome to {formatMessage({ id: getTranslation('plugin.name') })}</h1>
-    </Main>
+    <main>
+      <h1>Auth Manager</h1>
+    </main>
   );
 };
 
